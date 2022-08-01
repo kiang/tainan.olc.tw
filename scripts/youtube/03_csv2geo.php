bec086db5bc0ff293f50525b2050ada3a864119c
kiang <kiange@gmail.com>
2022-08-01 20:58:05 +0800

    generate key maps

--- scripts/youtube/03_csv2geo.php
+++ scripts/youtube/03_csv2geo.php
@@ -5,7 +5,7 @@ $youtubePath = $basePath . '/json/youtube';
 $listFile = $youtubePath . '/list.csv';
 $fh = fopen($listFile, 'r');
 fgetcsv($fh, 2048);
-$pool = [];
+$pool = $list = [];
 $fc = [
     'type' => 'FeatureCollection',
     'features' => [],
@@ -13,6 +13,12 @@ $fc = [
 while ($line = fgetcsv($fh, 2048)) {
     if (false !== strpos($line[1], '街講')) {
         if (!isset($pool[$line[2]])) {
+            $list[$line[2]] = [
+                [
+                    'id' => $line[0],
+                    'title' => $line[1],
+                ]
+            ];
             $pool[$line[2]] = [
                 'type' => 'Feature',
                 'properties' => [
@@ -29,9 +35,14 @@ while ($line = fgetcsv($fh, 2048)) {
             ];
         } else {
             ++$pool[$line[2]]['properties']['count'];
+            $list[$line[2]][] = [
+                'id' => $line[0],
+                'title' => $line[1],
+            ];
         }
     }
 }
 $fc['features'] = array_values($pool);
 
 file_put_contents($basePath . '/json/youtube.json', json_encode($fc, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+file_put_contents($basePath . '/json/youtube_list.json', json_encode($list, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
