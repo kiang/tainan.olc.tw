5d211bd512b00c2db2288c87e052887fc79cb923
kiang <kiange@gmail.com>
2022-08-01 19:43:50 +0800

    add latitude/longitude to youtube videos

--- scripts/youtube/02_json2csv.php
+++ scripts/youtube/02_json2csv.php
@@ -2,7 +2,21 @@
 $basePath = dirname(dirname(__DIR__));
 $youtubePath = $basePath . '/json/youtube';
 
-$fh = fopen($youtubePath . '/list.csv', 'w');
+$listFile = $youtubePath . '/list.csv';
+$pool = $locations = [];
+if (file_exists($listFile)) {
+    $fh = fopen($listFile, 'r');
+    fgetcsv($fh, 2048);
+    while ($line = fgetcsv($fh, 2048)) {
+        $pool[$line[0]] = $line;
+
+        if (!empty($line[3])) {
+            $locations[$line[2]] = [floatval($line[3]), floatval($line[4])];
+        }
+    }
+}
+
+$fh = fopen($listFile, 'w');
 fputcsv($fh, ['id', 'title', 'location', 'latitude', 'longitude']);
 foreach (glob($youtubePath . '/*.json') as $jsonFile) {
     $json = json_decode(file_get_contents($jsonFile), true);
@@ -10,6 +24,17 @@ foreach (glob($youtubePath . '/*.json') as $jsonFile) {
         if (!isset($item['id']['videoId'])) {
             continue;
         }
-        fputcsv($fh, [$item['id']['videoId'], $item['snippet']['title'], '', '', '']);
+        $location = $pool[$item['id']['videoId']][2];
+        $latitude = '';
+        $longitude = '';
+        if (isset($locations[$location])) {
+            $latitude = $locations[$location][0];
+            $longitude = $locations[$location][1];
+        }
+        if (isset($pool[$item['id']['videoId']])) {
+            fputcsv($fh, [$item['id']['videoId'], $item['snippet']['title'], $location, $latitude, $longitude]);
+        } else {
+            fputcsv($fh, [$item['id']['videoId'], $item['snippet']['title'], '', $latitude, $longitude]);
+        }
     }
 }
