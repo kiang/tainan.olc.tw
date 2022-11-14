92688ef1bf76a08f4c7bf5caf34dee3d8485e26d
kiang <kiange@gmail.com>
2022-11-15 00:47:28 +0800

    car kml/json

--- scripts/kml/01_kml2geojson.php
+++ scripts/kml/01_kml2geojson.php
@@ -16,3 +16,20 @@ foreach (glob($basePath . '/raw/kml/*.kml') as $kmlFile) {
         }
     }
 }
+
+$jsonPath = $basePath . '/raw/car_json';
+if (!file_exists($jsonPath)) {
+    mkdir($jsonPath, 0777, true);
+}
+foreach (glob($basePath . '/raw/car_kml/*.kml') as $kmlFile) {
+    $p = pathinfo($kmlFile);
+    $jsonFile = $jsonPath . '/' . $p['filename'] . '.geojson';
+    if (!file_exists($jsonFile)) {
+        exec("/home/kiang/.local/bin/k2g {$kmlFile} {$jsonPath}");
+        $styleFile = $jsonPath . '/style.json';
+        if (file_exists($styleFile)) {
+            copy($styleFile, $jsonFile);
+            unlink($styleFile);
+        }
+    }
+}
