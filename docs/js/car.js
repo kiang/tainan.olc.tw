9e9c199df793341e2a18621d82f49210622e1d33
kiang <kiange@gmail.com>
2022-11-17 21:40:08 +0800

    add click event

--- docs/js/car.js
+++ docs/js/car.js
@@ -51,38 +51,38 @@ var lineStyle = function (f) {
 function pointStyleFunction(f) {
     var p = f.getProperties(), stroke, radius;
     if (f === currentFeature) {
-      stroke = new ol.style.Stroke({
-        color: '#000',
-        width: 5
-      });
-      radius = 25;
+        stroke = new ol.style.Stroke({
+            color: '#000',
+            width: 5
+        });
+        radius = 25;
     } else {
-      stroke = new ol.style.Stroke({
-        color: '#fff',
-        width: 2
-      });
-      radius = 15;
+        stroke = new ol.style.Stroke({
+            color: '#fff',
+            width: 2
+        });
+        radius = 15;
     }
     let pointStyle = new ol.style.Style({
-      image: new ol.style.RegularShape({
-        radius: radius,
-        points: 3,
-        fill: new ol.style.Fill({
-          color: '#48c774'
+        image: new ol.style.RegularShape({
+            radius: radius,
+            points: 3,
+            fill: new ol.style.Fill({
+                color: '#48c774'
+            }),
+            stroke: stroke
         }),
-        stroke: stroke
-      }),
-      text: new ol.style.Text({
-        font: '14px "Open Sans", "Arial Unicode MS", "sans-serif"',
-        fill: new ol.style.Fill({
-          color: 'rgba(0,0,255,0.7)'
+        text: new ol.style.Text({
+            font: '14px "Open Sans", "Arial Unicode MS", "sans-serif"',
+            fill: new ol.style.Fill({
+                color: 'rgba(0,0,255,0.7)'
+            })
         })
-      })
     });
     pointStyle.getText().setText(p['地上層數']);
-    
+
     return pointStyle;
-  }
+}
 
 var lines = new ol.layer.Vector({
     source: new ol.source.Vector({
@@ -94,11 +94,11 @@ var lines = new ol.layer.Vector({
 
 var vectorPoints = new ol.layer.Vector({
     source: new ol.source.Vector({
-      url: 'https://kiang.github.io/tainan_basecode/bmc/points.json',
-      format: new ol.format.GeoJSON()
+        url: 'https://kiang.github.io/tainan_basecode/bmc/points.json',
+        format: new ol.format.GeoJSON()
     }),
     style: pointStyleFunction
-  });
+});
 
 var baseLayer = new ol.layer.Tile({
     source: new ol.source.WMTS({
@@ -150,6 +150,45 @@ var barContent = $('#sidebarContent');
 
 var geolocation = new ol.Geolocation({
     projection: appView.getProjection()
+});
+
+map.on('singleclick', function (evt) {
+    barContent.innerHTML = '';
+    pointClicked = false;
+    map.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
+        if (false === pointClicked) {
+            var p = feature.getProperties();
+            currentFeature = feature;
+            currentFeature.setStyle(pointStyleFunction(currentFeature));
+            if (false !== previousFeature) {
+                previousFeature.setStyle(pointStyleFunction(previousFeature));
+            }
+            previousFeature = currentFeature;
+            appView.setCenter(feature.getGeometry().getCoordinates());
+            var lonLat = ol.proj.toLonLat(p.geometry.getCoordinates());
+            var message = '<table class="table table-dark">';
+            message += '<tbody>';
+            message += '<tr><th scope="row" style="width: 80px;">名稱</th><td>' + p['管理委員會'] + '</td></tr>';
+            message += '<tr><th scope="row" style="width: 80px;">建築物地址</th><td>' + p['建築物地址'] + '</td></tr>';
+            message += '<tr><th scope="row" style="width: 80px;">棟數</th><td>' + p['棟數'] + '</td></tr>';
+            message += '<tr><th scope="row" style="width: 80px;">地上層數</th><td>' + p['地上層數'] + '</td></tr>';
+            message += '<tr><th scope="row" style="width: 80px;">地下層數</th><td>' + p['地下層數'] + '</td></tr>';
+            message += '<tr><th scope="row" style="width: 80px;">成立核准日期</th><td>' + p['成立核准日期'] + '</td></tr>';
+            message += '<tr><th scope="row" style="width: 80px;">成立核准文號</th><td>' + p['成立核准文號'] + '</td></tr>';
+            message += '<tr><th scope="row" style="width: 80px;">使用執照</th><td>' + p['使用執照'] + '</td></tr>';
+            message += '<tr><td colspan="2">';
+            message += '<hr /><div class="btn-group-vertical" role="group" style="width: 100%;">';
+            message += '<a href="https://www.google.com/maps/dir/?api=1&destination=' + lonLat[1] + ',' + lonLat[0] + '&travelmode=driving" target="_blank" class="btn btn-info btn-lg btn-block">Google 導航</a>';
+            message += '<a href="https://wego.here.com/directions/drive/mylocation/' + lonLat[1] + ',' + lonLat[0] + '" target="_blank" class="btn btn-info btn-lg btn-block">Here WeGo 導航</a>';
+            message += '<a href="https://bing.com/maps/default.aspx?rtp=~pos.' + lonLat[1] + '_' + lonLat[0] + '" target="_blank" class="btn btn-info btn-lg btn-block">Bing 導航</a>';
+            message += '</div></td></tr>';
+            message += '</tbody></table>';
+            barTitle.html(p['管理委員會']);
+            barContent.html(message);
+            sidebar.open('home');
+            pointClicked = true;
+        }
+    });
 });
 
 geolocation.setTracking(true);
