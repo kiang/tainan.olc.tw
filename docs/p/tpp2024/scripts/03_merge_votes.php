1b93aec5fe62e88070cf390889129665e3d1e893
kiang <kiange@gmail.com>
2024-12-09 21:42:52 +0800

    add votes field

---
+++ docs/p/tpp2024/scripts/03_merge_votes.php
@@ -0,0 +1,33 @@
+<?php
+$basePath = dirname(__DIR__);
+
+// Load candidates data
+$candidates = json_decode(file_get_contents($basePath . '/candidates.json'), true);
+
+// Create lookup array for candidates
+$candidateLookup = [];
+foreach ($candidates as $index => $candidate) {
+    $key = $candidate['選舉區'] . '_' . $candidate['號次'];
+    $candidateLookup[$key] = $index;
+}
+
+// Read and process votes.csv
+$fh = fopen($basePath . '/votes.csv', 'r');
+$header = fgetcsv($fh);
+
+while ($row = fgetcsv($fh)) {
+    $key = $row[0] . '_' . $row[2];
+    if (isset($candidateLookup[$key])) {
+        $index = $candidateLookup[$key];
+        $candidates[$index]['votes'] = intval($row[3]);
+    }
+}
+fclose($fh);
+
+// Save updated candidates data
+file_put_contents(
+    $basePath . '/candidates.json', 
+    json_encode($candidates, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+);
+
+echo "Votes data merged successfully!\n"; 
